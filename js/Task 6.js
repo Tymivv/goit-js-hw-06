173e44fb7cbb635c593bb02048ae6ef76b20820a
Tymivv <if210683tvn@gmail.com>
2021-11-11 14:39:55 +0200

    Update Task 6.js

--- js/Task 6.js
+++ js/Task 6.js
@@ -3,12 +3,16 @@ const inp = document.querySelector('#validation-input');
 inp.addEventListener('blur', valided);
 
 function valided() {
-    
-    if (inp.value.length == inp.dataset.length) {
-      inp.classList.add('valid');
-    } else {
-      inp.classList.add('invalid');
-    }
- 
+  if (inp.value.length === +inp.dataset.length) {
+    resetClass('valid', 'invalid');
+  } else {
+   resetClass('invalid', 'valid');
 }
+}
+
+function resetClass(addcl, delcl) {
+  inp.classList.add(addcl);
+  inp.classList.remove(delcl);
+}
+
 
